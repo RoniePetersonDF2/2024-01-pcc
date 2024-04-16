63628b2b85bf92366071e187c2041021353b5202
roniepetersondf <roniepetersondf@gmail.com>
2024-04-16 09:48:38 -0300

    Add query e dados relato

--- fontes/index.php
+++ fontes/index.php
@@ -1,7 +1,10 @@
 <?php
-    $dsn = 'mysql:host=localhost;dbname=ouvir_etc_db;charset=utf8;';
-    $conn = new PDO($dsn, 'root', '');
-    $conn = null;
+$dsn = 'mysql:host=localhost;dbname=ouvir_etc_db;charset=utf8;';
+$conn = new PDO($dsn, 'root', '');
+$query = 'SELECT * FROM ouvir_etc_db.relatos;';
+$stmt = $conn->query($query);
+$relatos = $stmt->fetchAll(PDO::FETCH_BOTH);
+$conn = null;
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
@@ -19,12 +22,23 @@
         <img src="assets/img/logoetc.png" alt="Imagem logo etc">
 
         <h1>Ouvir <span>ETC</span></h1>
-
-
     </header>
 
     <main>
+        <ul>
+            <?php foreach ($relatos as $relato) : ?>
+                <li>
+                    <h3>
+                        <?= $relato['titulo'] ?>
+                    </h3>
 
+                    <p>
+                        <?= $relato['descricao'] ?>
+                    </p>
+                    <a href="#">Editar</a>
+                </li>
+            <?php endforeach ?>
+        </ul>
 
     </main>
 
